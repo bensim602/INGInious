5eddc04deaf5f6ce50d1c2baa2df24ba21c84454
bensim602 <benjamin.simon@student.uclouvain.be>
2020-03-31 21:33:01 +0200

    [Course structure]: frontend for student view of course divided in section, button to switch between normal and section view. Lots of commonalities with the code for the course page, differences are marked with # [Source code integration]

--- inginious/frontend/plugins/course_structure/__init__.py
+++ inginious/frontend/plugins/course_structure/__init__.py
@@ -1,2 +1,15 @@
-def init(_, _2, _3, _4):
-    pass
+# -*- coding: utf-8 -*-
+#
+# This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
+# more information about the licensing of this file.
+from inginious.frontend.plugins.course_structure.course_page import CoursePage
+
+
+def switch_view(course, template_helper):
+    return str(template_helper.get_custom_renderer('frontend/plugins/course_structure', layout=False).switch_view(course))
+
+
+def init(plugin_manager, _, _2, _3):
+    """ Init the plugin """
+    plugin_manager.add_hook('course_menu', switch_view)
+    plugin_manager.add_page('/course_section/([^/]+)', CoursePage)
