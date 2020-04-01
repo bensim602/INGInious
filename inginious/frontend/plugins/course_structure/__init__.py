2de36374bd480a752e881f988a65bb281c1bbdc8
bensim602 <benjamin.simon@student.uclouvain.be>
2020-04-01 12:09:31 +0200

    [Course structure]: base for structure editor: display structure and possibility to save but no possibility to modify the structure. Use of a separate js file.

--- inginious/frontend/plugins/course_structure/__init__.py
+++ inginious/frontend/plugins/course_structure/__init__.py
@@ -2,14 +2,38 @@
 #
 # This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
 # more information about the licensing of this file.
+import os
+import web
+
 from inginious.frontend.plugins.course_structure.course_page import CoursePage
+from inginious.frontend.plugins.course_structure.structure_editor import CourseEditor
 
 
 def switch_view(course, template_helper):
     return str(template_helper.get_custom_renderer('frontend/plugins/course_structure', layout=False).switch_view(course))
 
 
+def course_admin_menu(course):
+    return "structure_editor", "<i class='fa fa-pencil fa-fw'></i>&nbsp; " + _("Structure editor")
+
+class StaticMockPage(object):
+    """Get the local javascript file"""
+    def GET(self):
+        try:
+            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "structure_editor.js"), 'rb') as file:
+                return file.read()
+        except:
+            raise web.notfound()
+
+    def POST(self):
+        return self.GET()
+
+
 def init(plugin_manager, _, _2, _3):
     """ Init the plugin """
     plugin_manager.add_hook('course_menu', switch_view)
     plugin_manager.add_page('/course_section/([^/]+)', CoursePage)
+
+    plugin_manager.add_hook("course_admin_menu", course_admin_menu)
+    plugin_manager.add_page('/admin/([^/]+)/structure_editor', CourseEditor)
+    plugin_manager.add_page('/plugins/course_structure/structure_editor.js', StaticMockPage)
---
+++ inginious/frontend/plugins/course_structure/structure_editor.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
+# more information about the licensing of this file.
+
+# [Source code integration]: move file to inginious.frontend.pages.course_admin, register page,
+# add link for course admin menu
+import json
+import web
+
+from inginious.frontend.pages.course_admin.utils import INGIniousAdminPage
+from inginious.frontend.plugins.course_structure.webapp_course import get_course_structure, check_toc, update_toc_content
+
+
+class CourseEditor(INGIniousAdminPage):
+    """ Couse structure editor """
+
+    def GET_AUTH(self, courseid):  # pylint: disable=arguments-differ
+        """ GET request """
+        course, __ = self.get_course_and_check_rights(courseid, allow_all_staff=False)
+        return self.page(course)
+
+    def POST_AUTH(self, courseid):  # pylint: disable=arguments-differ
+        """ POST request """
+        course, __ = self.get_course_and_check_rights(courseid, allow_all_staff=False)
+
+        errors = []
+        try:
+            user_input = web.input()
+            new_toc = json.loads(user_input["course_structure"])
+
+            valid, message = check_toc(course, new_toc)
+            if valid:
+                update_toc_content(self.course_factory, courseid, new_toc)
+            else:
+                errors.append(message)
+        except:
+            errors.append(_("Something wrong happened"))
+
+        if len(errors) == 0:
+            errors = None
+
+        return self.page(course, errors, errors is None)
+
+    def page(self, course, errors=None, validated=False):
+        """ Get all data and display the page """
+        tasks = course.get_tasks()
+        toc = get_course_structure(course, [])
+        return self.template_helper.get_custom_renderer('frontend/plugins/course_structure').structure_editor(course,
+                                                                                                              toc,
+                                                                                                              tasks,
+                                                                                                              errors,
+                                                                                                              validated)
